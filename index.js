6f2bbd9323fa0555e993be42b559a4df35758104
Tony <125552595+ThatCodebyTony@users.noreply.github.com>
2024-06-25 19:53:01 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+// Comment
+console.log('Log hello world');
+
+let name = ' Mosh';
+console.log(name);
+
+let firstName = "John"
+let lastName = "Smith"
+
+// const interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
+
+let age = 30;
+
+let isApproved = true;
+let firstN = undefined
+let lastN = null;
+let selectedColor = null;
+// let selectedColor = 'red';
+
+let person = {
+    name: 'John',
+    age: 30
+};
+
+// Dot notation
+person.name = 'Mary';
+
+// Bracket notation
+let selection = 'name';
+person[selection] = 'Harry';
+
+
+console.log(person.name);
+
+let selectedColors = ['red', 'blue'];
+selectedColors[2] = 'green';
+selectedColors[3] = 1;
+console.log(selectedColors);
+console.log(selectedColors.length);
+
+function greet(name, lastName) {
+    console.log('Hello function');
+}
+
+greet()
+
+function greet(name, lastName) {
+    console.log('Hello ' + name + ' ' + lastName);
+}
+
+greet('John', 'Smith');
+greet('Mary');
+
+function square(number) {
+    return number * number;
+}
+
+let number = square(3);
+
+console.log(number + " is the square of 3");
